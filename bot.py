f088b7fdf2d3b5db8d1f85efab512a789fbed8bc
Artem Tkachenko <133564785+derzilla@users.noreply.github.com>
2023-11-26 11:55:33 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,24 @@
+import asyncio
 import logging
 from aiogram import Bot
 from aiogram import types
 from aiogram import Dispatcher
 
+token = 'your token'
+bot = Bot(token=token)
+dp = Dispatcher()
 
+@dp.message
+async def saveis(message: types.Message):
+
+	txt = message.text
+	if txt == '/saveis':
+		await message.reply('We must save the Earth from global warming by any means possible, so that we can live better'
+							
+async def main():
+	
+	logging.basicConfig(level=logging.INFO)
+	await dp.start_polling(bot)
+	
+if __name__ == '__main__':
+	asyncio.run(main())
